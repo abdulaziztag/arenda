be0fcba04e8f5ca3e2fe173ea73472dbb23e8341
Erkin <esultonovv@gmail.com>
2021-07-19 17:43:58 +0500

    login 1st prototype

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <h1 class="lol">
-      <div class="lol">Login</div>
-    </h1>
-    <b-btn to="/register">register</b-btn>
+  <div class="login-main">
+    <div class="login-card">
+      <div class="login-top-section">
+        <h1 class="login-header">LogIn</h1>
+        <div class="input-block">
+          <div class="input-name">
+            Phone Number
+          </div>
+          <div class="input-icon-group">
+            <lable></lable>
+            <input type="text" placeholder="Type your phone number">
+            <div class="input-icon"><b-icon icon="phone"></b-icon></div>
+          </div>
+
+          <div class="input-name">
+            Password
+          </div>
+          <div class="input-icon-group last">
+            <input type="text" placeholder="Type your password">
+            <div class="input-icon"><b-icon icon="lock"></b-icon></div>
+          </div>
+          <div class="input-name-right">
+            <a href="">Forgot password?</a>
+          </div>
+        </div>
+        <button class="login-button">LOGIN</button>
+      </div>
+      <div class="login-bottom-section">
+        <a href="" class="sign-up-link">SIGN UP</a>
+      </div>
+    </div>
+<!--    <b-btn to="/register">register</b-btn>-->
   </div>
 </template>
 <script>
